26be8f1817b6ab41c013c3078b154c75f66787d2
Valiaaa <valialcc@gmail.com>
2023-05-23 22:21:14 -0400

    update

--- script.js
+++ script.js
@@ -8,6 +8,99 @@ $(document).ready(function(){
     var menuh = $('#menu').height();
     $('#ad').css("top", menuh + 14);
 
+
+
+    //final
+    $('#catgif').change(function(){
+        if ($(this).is(':checked')) {
+            $(".catjail").show();
+        } else {
+            $(".catjail").hide();
+        }
+    });
+
+    $('#advertisement').change(function(){
+        if ($(this).is(':checked')) {
+            $("#ad2").css({"opacity": "1" , 'transition': 'none'});
+        } else {
+            $("#ad2").css({"opacity": "0" , 'transition': 'none'});
+        }
+    });
+
+    $("#bgred, #bggreen, #bgblue").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var redValue = $("#bgredvalue").val().toString();
+        var greenValue = $("#bggreenvalue").val().toString();
+        var blueValue = $("#bgbluevalue").val().toString();
+        var color = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+        $(":root").css("--bg", color);
+    });
+
+    $("#titlefont").each(function(){
+        $(this).on('change',function updateSliderValue() {
+            output = $(this).val();
+            $(output).val($(this).val());
+            $('#title2').css('font-family', output);
+        });
+    });
+
+    $("#titlered, #titlegreen, #titleblue").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var redValue = $("#titleredvalue").val().toString();
+        var greenValue = $("#titlegreenvalue").val().toString();
+        var blueValue = $("#titlebluevalue").val().toString();
+        var color = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+        $("#title2").css("color", color);
+    });
+
+    $("#titlesize").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var size = $("#titlesizevalue").val().toString();
+        $("#title2").css({'font-size': size + "px"});
+    });
+
+    $("#titlescale").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var size = $("#titlescalevalue").val().toString();
+        var realsize = size / 100
+        $("#title2").css({'transform': 'scaleX(' + realsize + ")", 'transform-origin': 'top left'});
+    });
+
+    $("#titletracking").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var size = $("#titletrackingvalue").val().toString();
+        $("#title2").css({'letter-spacing': size + 'px'});
+    });
+
+    $("#titleleading").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var size = $("#titleleadingvalue").val().toString();
+        $("#title2").css({'line-height': size + 'px'});
+    });
+
+    $("#titletop").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var size = $("#titletopvalue").val().toString();
+        $("#title2").css({'margin-top': size + 'px'});
+    });
+
+    $("#titleleft").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var size = $("#titleleftvalue").val().toString();
+        $("#title2").css({'margin-left': size + 'px'});
+    });
+
+
+
+
     //stretched
     $("#stretchsl").on('change',function updateSliderValue() {
         var output = $(this).siblings("output").first();
@@ -144,29 +237,6 @@ $(document).ready(function(){
             $("#poort").css("font-family","freight-big-pro");
         }
 
-        if ($("#hy12").is(':checked') && $("#hy23").is(':checked')) {
-            $("#hyphengif").attr('src',"pic/party.gif");
-            $("#hyphen").addClass("strike")
-        } else {
-            $("#hyphengif").attr('src',"pic/bomb.gif");
-        }
-    });
-
-
-    //bad
-    $("input[type='radio'][name='c']").change(function() {
-        if ($("#poor1").is(':checked')) {
-            $("#poort").css("font-family","comic sans ms");
-        } else if ($("#poor2").is(':checked')) {
-            $("#poort").css("font-family","papyrus");
-        } else if ($("#poor3").is(':checked')) {
-            $("#poort").css("font-family","Brush Script MT");
-        } else if ($("#poor4").is(':checked')) {
-            $("#poort").css("font-family","futura");
-        } else if ($("#poor5").is(':checked')) {
-            $("#poort").css("font-family","freight-big-pro");
-        }
-
         if ($("#poor5").is(':checked')) {
             $("#poorgif").attr('src',"pic/party.gif");
             $("#poor").addClass("strike")
@@ -176,25 +246,65 @@ $(document).ready(function(){
     });
 
 
-
-
-    //final
-    $(".slider").on('change',function updateSliderValue() {
-        var therangeoutput = $(this).siblings("output").first();
-        $(therangeoutput).val($(this).val());
-        var redValue = $("#redrangevalue").val().toString();
-        var greenValue = $("#greenrangevalue").val().toString();
-        var blueValue = $("#bluerangevalue").val().toString();
-        var theColour = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
-        $(":root").css("--bg", theColour);
+    //width
+    $("input[type='radio'], input[type='range'], input[type='checkbox']").change(function() {
+        var width = $("#widthvalue").val().toString();
+        $(".widtht").css('width', width + "%");
     });
+
+    $("#widthsl").on('change',function updateSliderValue() {
+        var output = $(this).siblings("output").first();
+        $(output).val($(this).val());
+        var width = $("#widthvalue").val().toString();
+        $(".widtht").css('width', width + "%");
+
+        if ($('#widthvalue').val() >= '30' && $('#widthvalue').val() <= '50') {
+            $("#widthgif").attr('src',"pic/party.gif");
+            $("#width").addClass("strike")
+        } else {
+            $("#widthgif").attr('src',"pic/bomb.gif");
+        }
+    });
+
+    // $('#widthcheck1').change(function(){
+    //     if ($(this).is(':checked')) {
+    //         $(".widtht").css('text-indent', '30px');
+    //     } else {
+    //         $(".widtht").css('text-indent', '0');
+    //     }
+    // });
+
+    // $('#widthcheck2').change(function(){
+    //     if ($(this).is(':checked')) {
+    //         $(".widtht").css('margin-bottom', '20px');
+    //     } else {
+    //         $(".widtht").css('margin-bottom', '0');
+    //     }
+    // });
+
+    // $('#widthsl, #widthcheck1, #widthcheck2').change(function(){
+    //     if ($('#widthcheck1').is(':checked') && $('#widthcheck2').is(':checked')){
+    //         show1 = false
+    //     } else if ($('#widthcheck1').is(':checked') || $('#widthcheck2').is(':checked')) {
+    //         show1 = true
+    //     } else {
+    //         show1 = false
+    //     }
+
+    //     if (show1 && show2) {
+    //         $("#widthgif").attr('src',"pic/party.gif");
+    //         $("#width").addClass("strike")
+    //     } else {
+    //         $("#widthgif").attr('src',"pic/bomb.gif");
+    //     }
+    // });
 
 
     //check final
     var show = true;
 
     $("input[type='radio'], input[type='range'], input[type='checkbox']").change(function() {
-        if ($('#stretch').hasClass("strike") && $('#skew').hasClass("strike") && show) {
+        if ($('#stretch').hasClass("strike") && $('#skew').hasClass("strike") && $('#hang').hasClass("strike") && $('#hyphen').hasClass("strike") && $('#poor').hasClass("strike") && $('#width').hasClass("strike") && $('').hasClass("strike") && show) {
             setTimeout(function()
             {alert("Final Challenge Unlocked!");},40);
             $("#final, #final a").css({"color": "red", "pointer-events": "auto", "text-decoration": "underline", "text-decoration-thickness":"1.5px"});
